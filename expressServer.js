bb01f2aa7d08fd7df66a7e51fba4bca1ffdac7cf
Joe Lin <jlin249@my.bcit.ca>
2024-05-09 14:04:30 -0700

    removed the black borders from the header buttons

--- expressServer.js
+++ expressServer.js
@@ -13,7 +13,7 @@ const server = require('http').createServer(app)
 const whitelist = ['http://localhost:3000']
 
 app.use(cors({ credentials: true, origin: whitelist }))
-app.use(helmet())
+//app.use(helmet())
 app.use(compression())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
