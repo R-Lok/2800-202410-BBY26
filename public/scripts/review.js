8ecb615bab13ee2076372f8062b2bfaf68fa1184
Ryan Lok <rlok.pc@gmail.com>
2024-05-09 11:03:04 -0700

    Added eventListener that unflips cards when moving to the next card

--- public/scripts/review.js
+++ public/scripts/review.js
@@ -24,17 +24,43 @@ function decrementCounter() {
     }
 }
 
+function resetCards(e) {
+        const flippedCaption = document.querySelector(".carousel-caption[flipped]")
+
+        if(flippedCaption) {
+            flippedCaption.removeAttribute("flipped")
+            flippedCaption.childNodes.forEach(child => {
+                if(child.nodeType == 1) {
+                    child.classList.toggle("hidden")
+                }
+            })
+        }
+}
+
 const captions = document.querySelectorAll(".carousel-caption")
 console.log(captions)
+
 document.querySelector(".carousel-control-next").addEventListener("click", incrementCounter)
 document.querySelector(".carousel-control-prev").addEventListener("click", decrementCounter)
+document.querySelector(".carousel-control-next").addEventListener("click", resetCards)
 
 //make each flashcard flippable
 captions.forEach(element => {
     element.addEventListener("click", (e) => {
         console.log("flip")
-        const target = e.target.tagName == "H5" || "p" ? e.target.parentNode : e.target
-        target.querySelector("p").classList.toggle("hidden")
-        target.querySelector("h5").classList.toggle("hidden")
+        const target = e.target.tagName == "H5" || e.target.tagName == "P" ? e.target.parentNode : e.target
+        console.log(target)
+        const answer = target.querySelector('p')
+        const prompt = target.querySelector("h5")
+
+        answer.classList.toggle("hidden")
+        prompt.classList.toggle("hidden")
+
+        if (prompt.classList.contains("hidden")) {
+            target.setAttribute("flipped", "")
+        } else {
+            target.removeAttribute("flipped")
+        }
     })
 })
+
