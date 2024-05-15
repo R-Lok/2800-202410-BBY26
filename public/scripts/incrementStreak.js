408160c1f273a943a5e9143ac36730cd67fd4c35
LouiseYLi <louiseli.van@gmail.com>
2024-05-15 16:24:35 -0700

    prevActivityDay, the last day the user did an activity, is retrieved from user db, and compared to currActivityDate, the current date to see if its consecutive dates. if it is, then streak is incremented

--- public/scripts/incrementStreak.js
+++ public/scripts/incrementStreak.js
@@ -7,31 +7,36 @@ console.log('incrementStreak.js loaded')
 const incrementStreak = async(req) => {
     try {
         let shareId = req.params.setid;
-        let currentActivityDate = new Date();
+        let date = new Date();
+        let currActivityDate = date.getDate();
 
         let user = await usersModel.findOne({ loginId: req.session.loginId });
-        let lastActivity = user.lastActivity
-        let prevDate = lastActivity.timestamp.getDate();
-        console.log(`${prevDate}`)
-        user = await usersModel.findOneAndUpdate(
-            { loginId: req.session.loginId },
-            { $set: {
-                'lastActivity.timestamp': currentActivityDate,
-                'lastActivity.shareId': shareId
-            }},
-            {returnOriginal: false}
-        );
-        
-        if (user.lastActivity.timestamp.getDate() != prevDate) {
-            //reset here
-        } else {
-            // increment
-        }
+        let lastActivity = user.lastActivity;
+
+        let prevActivityDate = lastActivity.timestamp.getDate();
+        console.log(`prev ${prevActivityDate} curr ${currActivityDate}`)
+
+        // reset or increment streak
+        if (currActivityDate === prevActivityDate + 1) {
+            user = await usersModel.findOneAndUpdate(
+                { loginId: req.session.loginId },
+                {
+                    $set: {
+                        'lastActivity.timestamp': date,
+                        'lastActivity.shareId': shareId,
+                    },
+                    $inc: {
+                        streak: 1
+                    }
+                },
+                {returnOriginal: false}
+            );
+        } 
         if (!user) {
             throw console.error();
         }
         console.log(`after:${user}`);
-        // log currentActivityDate - will use later to check if date changed (eg 3->4)
+        // log currActivityDate - will use later to check if date changed (eg 3->4)
         // if diff date than before, inc, as long as consecutive day
         // what update to db: user.streak
         await user.save();
