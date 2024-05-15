b6b726b2a29a432e049dc51b051029bab9b29ed8
LouiseYLi <louiseli.van@gmail.com>
2024-05-15 15:40:32 -0700

    lastActivity for each user now gets automatically updated when /review route is accessed

--- public/scripts/incrementStreak.js
+++ public/scripts/incrementStreak.js
@@ -6,12 +6,30 @@ console.log('incrementStreak.js loaded')
 
 const incrementStreak = async(req) => {
     try {
-        const currentDate = new Date();
-        console.log(`login: ${req.session.loginId}\ndate: ${currentDate}`)
+        let shareId = req.params.setid;
+        let currentDate = new Date();
+
+        let user = await usersModel.findOneAndUpdate(
+            { loginId: req.session.loginId },
+            { $set: {
+                'lastActivity.timestamp': currentDate,
+                'lastActivity.shareId': shareId
+            }},
+            {returnOriginal: false}
+        );
+        if (!user) {
+            throw console.error();
+        }
+        console.log(`after:${user}`);
+        // log currentdate - will use later to check if date changed (eg 3->4)
+        // if diff date than before, inc, as long as consecutive day
+        // what update to db: user.streak
+        await user.save();
     } catch (err) {
         console.log(`error occured`)
     }
     console.log(`outside of try catch`)
+    return;
 }
 
 module.exports = { incrementStreak }
