8a0587d5c54a5ac24eda60b4fd4902a873251b50
LouiseYLi <louiseli.van@gmail.com>
2024-05-15 16:43:33 -0700

    cleaned up incrementStreak.js

--- public/scripts/incrementStreak.js
+++ public/scripts/incrementStreak.js
@@ -1,8 +1,7 @@
 require('dotenv').config({ path: `${process.cwd()}/.env.${process.env.NODE_ENV}` })
-const mongoose = require('mongoose')
 const usersModel = require('../../models/users')
 
-console.log('incrementStreak.js loaded')
+// console.log('incrementStreak.js loaded')
 
 const incrementStreak = async(req) => {
     try {
@@ -11,8 +10,10 @@ const incrementStreak = async(req) => {
         let currActivityDate = date.getDate();
 
         let user = await usersModel.findOne({ loginId: req.session.loginId });
+        if (!user) {
+            throw console.error();
+        }
         let lastActivity = user.lastActivity;
-
         let prevActivityDate = lastActivity.timestamp.getDate();
         console.log(`prev ${prevActivityDate} curr ${currActivityDate}`)
 
@@ -20,15 +21,13 @@ const incrementStreak = async(req) => {
         if (currActivityDate === prevActivityDate + 1) {
             user = await usersModel.findOneAndUpdate(
                 { loginId: req.session.loginId },
-                {
-                    $set: {
+                {$set: {
                         'lastActivity.timestamp': date,
                         'lastActivity.shareId': shareId,
-                    },
-                    $inc: {
-                        streak: 1
-                    }
                 },
+                $inc: {
+                    streak: 1
+                }},
                 {returnOriginal: false}
             );
         } else if (currActivityDate === prevActivityDate) {
@@ -51,18 +50,13 @@ const incrementStreak = async(req) => {
                 {returnOriginal: false}
             );
         }
-        if (!user) {
-            throw console.error();
-        }
         console.log(`after:${user}`);
-        // log currActivityDate - will use later to check if date changed (eg 3->4)
-        // if diff date than before, inc, as long as consecutive day
-        // what update to db: user.streak
         await user.save();
     } catch (err) {
-        console.log(`error occured`)
+        console.log(`error occured`);
+        return;
     }
-    console.log(`outside of try catch`)
+    console.log(`outside of try catch`);
     return;
 }
 
