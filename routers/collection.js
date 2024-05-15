7e282f4f7c7154837c98f1539d05c86a34302999
Joe Lin <jlin249@my.bcit.ca>
2024-05-15 14:09:54 -0700

    Added authorization for collections deletion and 403.ejs file, and added deletion of flashcards as well as flashcards set

--- routers/collection.js
+++ routers/collection.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = new express.Router();
 const collectionsModel = require('../models/collections')
+const flashcardsModel = require('../models/flashcards')
 
 router.get('/', async (req, res) => {
-    let userId = req.session.userId;
+    let userId = req.session.userId
     const collections = await collectionsModel.find({ userId: userId });
     return res.render('collection', { collections: collections })
 })
 
 router.post('/search', async (req, res) => {
-    let userId = req.session.userId;
+    const userId = req.session.userId
     const search = req.body.search
     const regexPattern = new RegExp('^' + search, 'i')
     const collections = await collectionsModel.find({ userId: userId, setName: { $regex: regexPattern } })
@@ -17,20 +18,28 @@ router.post('/search', async (req, res) => {
 })
 
 router.get('/delete/:shareid', async (req, res) => {
-    const shareId = req.params.shareid
-    console.log('Inside delete, shareid: ' + shareId)
+    const userID = req.session.userId
+    const shareID = req.params.shareid
+    const setOwnerId = await collectionsModel.findOne({shareId:shareID}).select('userId');
 
-    async function deleteSet(shareID) {
-        try {
-            await collectionsModel.deleteOne({ shareId: shareID })
-            console.log('Document deleted successfully')
-        } catch (err) {
-            console.error('Error deleting document: ', err)
+    if(userID != setOwnerId.userId) {
+        let error = res.status(403);
+        res.render('403', {error:"User Not Authorized"});
+    } else {
+
+        async function deleteSet(shareID) {
+            try {
+                await collectionsModel.deleteOne({ shareId: shareID })
+                await flashcardsModel.deleteMany({shareId: shareID})
+                console.log('Document deleted successfully')
+            } catch (err) {
+                console.error('Error deleting document: ', err)
+            }
         }
-    }
 
-    await deleteSet(shareId)
-    res.redirect('/collection')
+        await deleteSet(shareID)
+        res.redirect('/collection')
+    }
 })
 
 module.exports = router;
