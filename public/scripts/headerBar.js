0c596a18ef986154ab2da982114baa19272cf486
Will Liao <wyliao76@gmail.com>
2024-05-15 15:25:12 -0700

    fix navbar

---
+++ public/scripts/headerBar.js
@@ -0,0 +1,8 @@
+const homeBtn = document.getElementById('homeBtn')
+homeBtn.onclick = function() {
+    window.location.href = '/'
+}
+const settingsBtn = document.getElementById('settingsBtn')
+settingsBtn.onclick = function() {
+    window.location.href = '/settings'
+}
