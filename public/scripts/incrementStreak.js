918230974f8bbcb0ce71ed423c15c5a62e96430e
LouiseYLi <louiseli.van@gmail.com>
2024-05-16 13:46:30 -0700

    modified incrementStreak.js, incrementStreak.js now does not reset the streak

--- public/scripts/incrementStreak.js
+++ public/scripts/incrementStreak.js
@@ -5,7 +5,7 @@ const incrementStreak = async(req) => {
     try {
         let shareId = req.params.setid;
         let date = new Date();
-        let currActivityDate = date.getDate() + 1;
+        let currActivityDate = date.getDate();
 
         let user = await usersModel.findOne({ loginId: req.session.loginId });
         if (!user) {
@@ -13,7 +13,8 @@ const incrementStreak = async(req) => {
         }
         let lastActivity = user.lastActivity;
         let prevActivityDate = lastActivity.timestamp.getDate();
-
+        console.log(`user: ${user}`)
+        console.log(`prev date: ${prevActivityDate}\n curr date: ${currActivityDate}`)
         // reset or increment streak
         if (currActivityDate === prevActivityDate + 1) {
             user = await usersModel.findOneAndUpdate(
@@ -28,21 +29,13 @@ const incrementStreak = async(req) => {
                 {returnOriginal: false}
             );
         } else if (currActivityDate === prevActivityDate) {
-            user = await usersModel.findOneAndUpdate(
-                { loginId: req.session.loginId },
-                { $set: {
-                    'lastActivity.timestamp': date,
-                    'lastActivity.shareId': shareId
-                }},
-                {returnOriginal: false}
-            );
-        } else {
+            let value = user.streak
             user = await usersModel.findOneAndUpdate(
                 { loginId: req.session.loginId },
                 { $set: {
                     'lastActivity.timestamp': date,
                     'lastActivity.shareId': shareId,
-                    streak: 0
+                    streak: value
                 }},
                 {returnOriginal: false}
             );
