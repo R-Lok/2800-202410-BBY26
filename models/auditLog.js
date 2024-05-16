2feb5a00328cebfeb7851e5daff49534bec8795d
LouiseYLi <louiseli.van@gmail.com>
2024-05-16 16:37:33 -0700

    adjusted type and added shareid attribute to audit log schema, auditLog.js

--- models/auditLog.js
+++ models/auditLog.js
@@ -3,8 +3,9 @@ const { Schema } = mongoose
 
 const AuditLogSchema = new Schema({
     loginId: {type: String, required: true, unique: true},
-    type: {type: String, default: null},
+    type: {type: String, default: null, enum: ['flashcard']},
     timestamp: {type: Date, default: null},
+    shareId: {type: Number, required: true, unique: true}
 }, {
     timestamps: true,
 })
