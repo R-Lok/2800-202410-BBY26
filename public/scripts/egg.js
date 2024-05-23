df87790e5436c084fdeea99bd0ec9a71a0ffe832
Joe Lin <jlin249@my.bcit.ca>
2024-05-23 01:01:26 -0700

    added audio and some extra styling to egg page

--- public/scripts/egg.js
+++ public/scripts/egg.js
@@ -1,6 +1,7 @@
 const eggPic = document.getElementById('eggPic')
 const studyGen = document.getElementById('studyGen')
 const spans = document.getElementsByTagName('span')
+const audio = document.getElementById('pickaxeAudio')
 
 eggPic.addEventListener('click', () => {
     if (eggPic.src.includes('/images/depresso2.gif')) {
@@ -9,6 +10,7 @@ eggPic.addEventListener('click', () => {
         eggPic.src = '/images/depresso2.gif'
         studyGen.classList.remove('eggText')
         studyGen.classList.add('eggText2')
+        audio.muted = true
 
         for (let i = 0; i < spans.length; i++) {
             if ((i % 2) === 0) {
@@ -21,3 +23,20 @@ eggPic.addEventListener('click', () => {
         }
     }
 })
+
+function initialPlayAudio() {
+    const delay = 1650
+    const delay2 = 1950
+
+    setTimeout(() => {
+        audio.play()
+    }, delay)
+
+    audio.addEventListener('ended', () => {
+        setTimeout(() => {
+            audio.play()
+        }, delay2)
+    })
+}
+
+window.onload = initialPlayAudio
