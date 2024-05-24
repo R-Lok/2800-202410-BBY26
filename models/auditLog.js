5579ec6549b7aaa39ee183016fb61692219f7d3c
Will Liao <wyliao76@gmail.com>
2024-05-24 16:58:13 -0700

    fix auditLog

--- models/auditLog.js
+++ models/auditLog.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const AuditLogsSchema = new Schema({
-    loginId: {type: String, required: true, unique: true, ref: 'users'},
-    type: {type: String, default: null, enum: ['flashcard']},
-    shareId: {type: Number, required: true, unique: true, ref: 'collections'},
+    loginId: {
+        type: String,
+        required: true,
+        ref: 'users',
+    },
+    type: {
+        type: String,
+        default: null,
+        enum: ['flashcard'],
+    },
+    shareId: {
+        type: Number,
+        required: true,
+        ref: 'collections',
+    },
 }, {
     timestamps: true,
 })
