fbe6aec01c88046756c054b9a0438ba9c63e6d7f
Joe Lin <jlin249@my.bcit.ca>
2024-05-27 21:59:59 -0700

    modified easter egg to open link on a separate window

--- public/scripts/collection.js
+++ public/scripts/collection.js
@@ -1,5 +1,5 @@
 document.getElementById('sortCollection').addEventListener('change', async function(e) {
-    const selectedOption = e.target.value;
+    const selectedOption = e.target.value
     try {
         const response = await fetch(`/collection/sortCollection`, {
             method: 'POST',
@@ -9,24 +9,21 @@ document.getElementById('sortCollection').addEventListener('change', async funct
             body: JSON.stringify({ selectedOption }),
         })
 
-        let collections = await response.json();
+        let collections = await response.json()
 
         if (response.ok) {
             collections = collections.collections
             let htmlString = ''
 
-            let collectionsList = document.getElementById("collectionsList");
-            for(let i = 0; i < collections.length; i++) {
+            const collectionsList = document.getElementById('collectionsList')
+            for (let i = 0; i < collections.length; i++) {
                 htmlString += `<div class="list-group-item d-flex list-group-item-action w-100">
                 <a href="/review/${collections[i].shareId}" class="p-0 m-0 flex-grow-1 setText">${collections[i].setName}</a>
-                <button class="btn btn-danger" onclick="confirm('Are you sure you want to delete this set?') ? location.href ='/collection/delete/` + collections[i].shareId + `' : event.preventDefault()">Delete</button>
+                <button class="btn btn-danger" onclick="confirm('Are you sure you want to delete this set?') ? location.href ='/collection/delete/` + collections[i].shareId +
+                `' : event.preventDefault()">Delete</button>
                 </div>`
             }
-            
-            collectionsList.innerHTML = htmlString;
-
-            
-            
+            collectionsList.innerHTML = htmlString
         }
     } catch (error) {
         console.log(error)
