d26f0b4b01cdb4e96f15d6d3a04785b7105d2612
Louise Li <louiseli.van@gmail.com>
2024-05-27 22:33:57 -0700

    added comments to isConsecutiveDays and incrementStreak from streak.js. also removed unused code from incrementStreak

--- public/scripts/streak.js
+++ public/scripts/streak.js
@@ -1,71 +1,46 @@
-// require('dotenv').config({ path: `${process.cwd()}/.env.${process.env.NODE_ENV}` })
 const usersModel = require('../../models/users')
 
+// isConsecutiveDays checks if two dates are consecutive dates
 function isConsecutiveDays(lastDate, currDate) {
-    // console.log(`\nisConsecutiveDays\n`)
-
     let d = new Date(lastDate.getFullYear(), lastDate.getMonth(), lastDate.getDate())
     let d2 = new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate())
-    // console.log(`lastDate: ${d}`)
-    // console.log(`currDate: ${d2}`)
-
-    // console.log(`${d.getTime()} ${d2.getTime()}`)
-
+    // Gets the millisecond difference between dates
     let msDifferenceBetweenDays = Math.abs(d.getTime() - d2.getTime())
-    let dayDifference = msDifferenceBetweenDays / (1000 * 60 * 60 * 24) // converting millisecond difference between dates to days
-
-    // console.log(`${dayDifference}`)
+    // Converts millisecond difference between dates to days
+    let dayDifference = msDifferenceBetweenDays / (1000 * 60 * 60 * 24) 
     return dayDifference
 }
 
+// incrementStreak checks if streak should be incremented or maintained 
 const incrementStreak = async(req) => {
     try {
-        let shareId = req.params.setid
-
         let user = await usersModel.findOne({ loginId: req.session.loginId })
         let date = new Date()
-        // date.setMonth(5)
-        // date.setDate(25) 
-        // console.log(`after setdate ${date.getDate()}`)
-        
-        let lastActivity = user.lastActivity;
-        // console.log(`lastActivity ${lastActivity.timestamp}`)
-
-        // If lastActivity or its properties are null, then set the properties and increment the streak
+        let lastActivity = user.lastActivity
+        // Checks if user's last activity is null or if the user's lastActivity time and current date are consecutive
         if ((lastActivity == null || lastActivity.timestamp == null || lastActivity.shareId == null)
             || (isConsecutiveDays(lastActivity.timestamp, date)) || (user.streak == 0)) {
+            // If user's last activity is null or consecutive to the current date, then update lastActivity and increment streak
             user = await usersModel.findOneAndUpdate(
                 { loginId: req.session.loginId },
-                {$set: {
-                        'lastActivity.timestamp': date,
-                        'lastActivity.shareId': shareId,
-                },
-                $inc: {
-                    streak: 1
-                }},
-                {returnOriginal: false}
+                { $set: { 'lastActivity.timestamp': date, 'lastActivity.shareId': req.params.setid, }, $inc: {streak: 1} },
+                { returnOriginal: false }
             );
-            await user.save();
-            return;
-        // else if the days are the same, then maintain
         } else if (isConsecutiveDays(lastActivity.timestamp, date) == 0) {
+            // If the user's last activity date and current date are the same, then update lastActivity and maintain the streak
             let value = user.streak
             user = await usersModel.findOneAndUpdate(
                 { loginId: req.session.loginId },
-                { $set: {
-                    'lastActivity.timestamp': date,
-                    'lastActivity.shareId': shareId,
-                    streak: value
-                }},
-                {returnOriginal: false}
+                { $set: { 'lastActivity.timestamp': date, 'lastActivity.shareId': req.params.setid, streak: value } },
+                { returnOriginal: false }
             );
         }
-        await user.save();
+        await user.save()
     } catch (err) {
-        console.log(`Error found: ${err}`);
-        return;
+        console.log(`Error found: ${err}`)
+        return
     }
-    return;
+    return
 }
 
 module.exports = { incrementStreak, isConsecutiveDays }
