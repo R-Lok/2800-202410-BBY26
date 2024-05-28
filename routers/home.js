16962a6ac8a83dedf74d8e660781258267e801ac
Louise Li <louiseli.van@gmail.com>
2024-05-27 23:18:08 -0700

    refactored code for home endpoint in home.js. created function to render homepage on home.js

--- routers/home.js
+++ routers/home.js
@@ -16,83 +16,35 @@ const {
 } = require('../public/scripts/calendar');
 
 router.get('/', async (req, res) => {
-    let existingActivity
+    const date = new Date()
+    let existingActivity = 0
     let activityName
-    let date = new Date()
-    let prevMonthDays = generateDaysOfPrevMonth(date)
-    let currMonthDays = generateDaysOfCurrMonth(date)
-    let nextMonthDays = generateDaysOfNextMonth(date)
-    let monthName = getMonthName(date)
-    let year = date.getFullYear()
     let auditLogResult
     let user
-
     try {
         let prevMonthLastDate = getPrevMonthLastDate(date)
         let calendarStartDate = prevMonthLastDate.getDate() - prevMonthLastDate.getDay()
         prevMonthLastDate.setDate(calendarStartDate)
-    
         auditLogResult = await auditLogsModel.find({ loginId: req.session.loginId, createdAt: { "$gte": prevMonthLastDate } })        
         user = await usersModel.findOne({ loginId: req.session.loginId })
-        let lastActivity = user.lastActivity
-        
-        if (lastActivity == null || lastActivity.timestamp == null || lastActivity.shareId == null) {
-            existingActivity = 0
-            return res.render('home', {
-                prevMonthDays: prevMonthDays,
-                currMonthDays: currMonthDays,
-                nextMonthDays: nextMonthDays,
-                monthName: monthName,
-                year: year, 
-                activityName: activityName,
-                existingActivity: existingActivity,
-                days: user.streak, 
-                studiedDays: getStudiedDays(auditLogResult),
-                streakDays: getStreakDays(user.lastActivity.timestamp, date, user.streak),
-                name: req.session.name,
-                email: req.session.email,
-                pictureID:req.session.picture
-            })
-        }
-        let dayDifference = isConsecutiveDays(lastActivity.timestamp, date)
-
-        // If dates are NOT consecutive (isConsecutiveDays == 1) AND NOT the same (isConsecutiveDays == 0),
-        // then reset the streak. 
+        if (user.lastActivity == null || user.lastActivity.timestamp == null || user.lastActivity.shareId == null) return renderHome(req, res, date, activityName, existingActivity, user, auditLogResult)
+        const dayDifference = isConsecutiveDays(user.lastActivity.timestamp, date)
+        // If dates are not consecutive and not the same, then reset the streak. 
         if ((dayDifference != 1) && (dayDifference != 0)) {
             user = await usersModel.findOneAndUpdate( { loginId: req.session.loginId },
-                { $set: {
-                    'lastActivity.timestamp': user.lastActivity.timestamp,
-                    'lastActivity.shareId': user.lastActivity.shareId,
-                    'streak': 0,
-                } }, { returnOriginal: false }, )
+                { $set: { 'lastActivity.timestamp': user.lastActivity.timestamp, 'lastActivity.shareId': user.lastActivity.shareId, 'streak': 0, } },
+                { returnOriginal: false }, )
             await user.save()
         }
-
-        const collection = await collectionsModel.findOne( { shareId: lastActivity.shareId } )
-        if (!collection) {
-            existingActivity = 0
-        } else {
-            existingActivity = `/review/${lastActivity.shareId}`
+        const collection = await collectionsModel.findOne( { shareId: user.lastActivity.shareId } )
+        if (collection) {
+            existingActivity = `/review/${user.lastActivity.shareId}`
             activityName = collection.setName
         }
     } catch (err) {
         console.log(`Error occurred in /home: ${err}`)
     }
-    return res.render('home', {
-        prevMonthDays: prevMonthDays,
-        currMonthDays: currMonthDays,
-        nextMonthDays: nextMonthDays,
-        monthName: monthName,
-        year: year,
-        activityName: activityName,
-        existingActivity: existingActivity,
-        days: user.streak,
-        studiedDays: getStudiedDays(auditLogResult),
-        streakDays: getStreakDays(user.lastActivity.timestamp, date, user.streak),
-        name: req.session.name,
-        email: req.session.email,
-        pictureID:req.session.picture
-    })
+    return renderHome(req, res, date, activityName, existingActivity, user, auditLogResult)
 })
 
 router.post('/shareCode', (req, res) => {
@@ -102,5 +54,23 @@ router.post('/shareCode', (req, res) => {
         res.redirect(`/review/${req.body.shareId}`)
     }
 })
+
+function renderHome(req, res, date, activityName, existingActivity, user, auditLogResult) {
+    return res.render('home', {
+        prevMonthDays: generateDaysOfPrevMonth(date),
+        currMonthDays: generateDaysOfCurrMonth(date),
+        nextMonthDays: generateDaysOfNextMonth(date),
+        monthName: getMonthName(date),
+        year: date.getFullYear(),
+        activityName: activityName,
+        existingActivity: existingActivity,
+        days: user.streak,
+        studiedDays: getStudiedDays(auditLogResult),
+        streakDays: getStreakDays(user.lastActivity.timestamp, date, user.streak),
+        name: req.session.name,
+        email: req.session.email,
+        pictureID:req.session.picture
+    })
+}
 
 module.exports = router
