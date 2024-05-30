e56ca52db2b2545c2557093c22b7cc0f13b3895f
Ryan Lok <rlok.pc@gmail.com>
2024-05-30 00:53:21 -0700

    Implemented 400 redirect if /check route contains invalid data

--- src/routers/check.js
+++ src/routers/check.js
@@ -4,10 +4,15 @@ const router = new express.Router()
 // this route receives json data from the generate page. The JSON represents the flashcards
 // and this route renders a "review" page to the client tolet them view the cards before saving them.
 router.get('/', (req, res) => {
-    const data = (JSON.parse(req.query.data)).flashcards
-    const carouselData = { bg: '/images/plain-FFFFFF.svg', cards: data, queryType: 'finalize', pictureID: req.session.picture }
+    try {
+        const raw = JSON.parse(req.query.data)
+        const data = raw.flashcards
+        const carouselData = { bg: '/images/plain-FFFFFF.svg', cards: data, queryType: 'finalize', pictureID: req.session.picture }
 
-    return res.render('review', carouselData)
+        return res.render('review', carouselData)
+    } catch {
+        return res.render('400', { error: 'Invalid data', pictureID: req.session.picture })
+    }
 })
 
 module.exports = router
